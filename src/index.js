a1ae5f4c466b9e94c5bf3a65be1d585622675f14
LeoUTas <hoang.nguyen0@utas.edu.au>
2024-01-08 17:05:05 -0330

    fix routing bug

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const container = document.querySelector("#root");
 
 const root = createRoot(container);
 root.render(
-    <BrowserRouter basename="/todo_today">
+    <BrowserRouter>
         <App />
     </BrowserRouter>
 );
