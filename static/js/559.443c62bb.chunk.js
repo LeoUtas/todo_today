b9ea68e434e69521ec0e0cfc3c215b2455a4174a
LeoUtas <59540161+LeoUtas@users.noreply.github.com>
2024-01-08 20:41:43 +0000

    Deploying to gh-pages from @ LeoUtas/todo_today@f1089db0dbcf5b5776e947197a0060a6e3596d8f 🚀

---
+++ static/js/559.443c62bb.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktodo_today=self.webpackChunktodo_today||[]).push([[559],{559:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=559.443c62bb.chunk.js.map
