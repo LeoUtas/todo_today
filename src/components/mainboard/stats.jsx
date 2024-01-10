2b99186d4efe552b7ed016959fa372e7e7c4650f
LeoUTas <hoang.nguyen0@utas.edu.au>
2024-01-09 23:48:12 -0330

    make new style for xaxis of the barplot lim from 0 to 365

--- src/components/mainboard/stats.jsx
+++ src/components/mainboard/stats.jsx
@@ -5,7 +5,6 @@ import "./mainboard.css";
 
 const MARGIN = { top: 30, right: 30, bottom: 30, left: 30 };
 const BAR_PADDING = 0.2;
-// const PLOT_LAYOUT = { width: 600, height: 500 };
 
 const Barplot = ({ width, height, data }) => {
     const boundsWidth = width - MARGIN.right - MARGIN.left;
@@ -32,18 +31,31 @@ const Barplot = ({ width, height, data }) => {
         tooltipRef.current.style.opacity = 0;
     };
 
+    const getDayOfYear = (dateString) => {
+        const date = new Date(dateString);
+        const start = new Date(date.getFullYear(), 0, 0);
+        const diff =
+            date -
+            start +
+            (start.getTimezoneOffset() - date.getTimezoneOffset()) * 60 * 1000;
+        const oneDay = 1000 * 60 * 60 * 24;
+        return Math.floor(diff / oneDay);
+    };
+
     const xScale = d3
-        .scaleBand()
-        .domain(data.map((data) => data.day))
-        .range([0, boundsWidth])
-        .padding(BAR_PADDING);
+        .scaleLinear()
+        .domain([0, 365]) // Setting domain from 0 to 365
+        .range([0, boundsWidth]);
 
     const yScale = d3.scaleLinear().domain([0, 100]).range([boundsHeight, 0]);
 
     const stackedBars = data.map((d, i) => {
-        const x = xScale(d.day);
+        const x = xScale(getDayOfYear(d.date));
         const heightDone = yScale(0) - yScale(d.done);
         const heightNotDone = yScale(0) - yScale(d.notdone);
+
+        // Calculate the bar width
+        const barWidth = boundsWidth / 365 - BAR_PADDING;
 
         return (
             <g key={i}>
@@ -51,7 +63,7 @@ const Barplot = ({ width, height, data }) => {
                 <rect
                     x={x}
                     y={boundsHeight - heightDone}
-                    width={xScale.bandwidth()}
+                    width={barWidth}
                     height={heightDone}
                     style={{
                         fill: "var(--color-primary)",
@@ -64,7 +76,7 @@ const Barplot = ({ width, height, data }) => {
                 <rect
                     x={x}
                     y={boundsHeight - heightDone - heightNotDone}
-                    width={xScale.bandwidth()}
+                    width={barWidth}
                     height={heightNotDone}
                     style={{
                         fill: "var(--color-secondary)",
@@ -99,6 +111,15 @@ const Barplot = ({ width, height, data }) => {
                         y2={boundsHeight}
                         className="x-axis-line"
                     />
+
+                    <text
+                        x={boundsWidth / 2} // middle of the x-axis
+                        y={boundsHeight + 25} // 20px below the x-axis
+                        textAnchor="middle" // Center the text
+                        className="x-axis-text"
+                    >
+                        Your 365 days
+                    </text>
                 </g>
             </svg>
             <div className="tooltip" ref={tooltipRef}>
